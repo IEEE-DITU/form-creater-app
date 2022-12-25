51b8bdefc70604e333f72b061ba4f230f60a9ed1
TheDarkLord100 <miracleyt404@gmail.com>
2022-12-26 00:17:12 +0530

    Fixed SignUp Screen UI

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'login_screen.dart';
 
@@ -10,121 +11,124 @@ class SignupScreen extends StatefulWidget {
 
 class _SignupScreenState extends State<SignupScreen> {
   bool _isHidden = true;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(children: [
-          const SizedBox(
-            height: 100.0,
-          ),
-          const SizedBox(
-            width: 500,
-            child: Text(
-              'Sign up',
-              style: TextStyle(
-                fontSize: 50,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 20.0,
-          ),
-          const SizedBox(
-            width: 500,
-            child: Text('Already have an account with us?',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                )),
-          ),
-          const SizedBox(
-            height: 10.0,
-          ),
-          SizedBox(
-              width: 500,
-              child: Row(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  const Text(
-                    'Skip to ',
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      debugPrint('login');
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const LoginScreen()));
-                    },
-                    child: const Text('Login !',
+              const SizedBox(
+                height: 100.0,
+              ),
+              const Text(
+                'Sign up',
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              const SizedBox(
+                height: 30.0,
+              ),
+              const Text('Already have an account with us?',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                  )),
+              const SizedBox(
+                height: 5,
+              ),
+              RichText(
+                  text: TextSpan(
+                    children: [
+                      const TextSpan(
+                        text: 'Skip to ',
                         style: TextStyle(
-                            fontWeight: FontWeight.w800, fontSize: 20)),
-                  )
-                ],
-              )),
-          const SizedBox(
-            height: 30.0,
+                          fontSize: 20,
+                          color: Colors.black
+                        )
+                      ),
+                      TextSpan(
+                        text: 'Login !',
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red
+                        ),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            debugPrint('login');
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const LoginScreen()));
+                          }
+                      )
+                    ]
+                  )),
+              const SizedBox(
+                height: 30.0,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.email_outlined),
+                    hintText: "Enter your email address",
+                    labelText: "Email"),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.person),
+                    hintText: " Enter your User name ",
+                    labelText: "Username"),
+              ),
+              TextFormField(
+                obscureText: _isHidden,
+                decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.lock),
+                    suffixIcon: InkWell(
+                      onTap: _togglePasswordView,
+                      child:
+                          Icon(_isHidden ? Icons.visibility : Icons.visibility_off),
+                    ),
+                    hintText: " Enter your Password",
+                    labelText: "Password"),
+              ),
+              TextFormField(
+                obscureText: _isHidden,
+                decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.lock),
+                    suffixIcon: InkWell(
+                      onTap: _togglePasswordView,
+                      child:
+                          Icon(_isHidden ? Icons.visibility : Icons.visibility_off),
+                    ),
+                    hintText: "Confirm your Password",
+                    labelText: "Confirmation Password"),
+              ),
+              const SizedBox(
+                height: 30.0,
+              ),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50)),
+                    minimumSize: const Size.fromHeight(50)
+                  ),
+                  child: const Text('Register'),
+                  onPressed: () {
+                    debugPrint('Register');
+                  })
+            ]),
           ),
-          TextFormField(
-            decoration: const InputDecoration(
-                prefixIcon: Icon(Icons.email_outlined),
-                hintText: " Enter your email address",
-                labelText: "Email"),
-          ),
-          TextFormField(
-            decoration: const InputDecoration(
-                prefixIcon: Icon(Icons.person),
-                hintText: " Enter your User name ",
-                labelText: "Username"),
-          ),
-          TextFormField(
-            obscureText: _isHidden,
-            decoration: InputDecoration(
-                prefixIcon: const Icon(Icons.lock),
-                suffixIcon: InkWell(
-                  onTap: _togglePasswordView,
-                  child:
-                      Icon(_isHidden ? Icons.visibility : Icons.visibility_off),
-                ),
-                hintText: " Enter your Password",
-                labelText: "Password"),
-          ),
-          TextFormField(
-            obscureText: _isHidden,
-            decoration: InputDecoration(
-                prefixIcon: const Icon(Icons.lock),
-                suffixIcon: InkWell(
-                  onTap: _togglePasswordView,
-                  child:
-                      Icon(_isHidden ? Icons.visibility : Icons.visibility_off),
-                ),
-                hintText: "Confirm your Password",
-                labelText: "Confirmation Password"),
-          ),
-          const SizedBox(
-            height: 30.0,
-          ),
-          SizedBox(
-            width: 300,
-            height: 40,
-            child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50)),
-                ),
-                child: const Text('Register'),
-                onPressed: () {
-                  debugPrint('Register');
-                }),
-          )
-        ]),
+        ),
       ),
     );
   }
