6995231b1a3df88658eb96aafdfe67e755981822
TheDarkLord100 <miracleyt404@gmail.com>
2023-01-29 16:14:32 +0530

    changed edit profile icon

--- lib/navigation/my_screen.dart
+++ lib/navigation/my_screen.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:ieee_forms/services/colors.dart';
 import 'package:ieee_forms/services/user.dart';
 import 'package:ieee_forms/widgets/custom_dialog.dart';
 
@@ -21,53 +23,65 @@ class _ProfileScreenState extends State<ProfileScreen> {
             padding: const EdgeInsets.all(16.0),
             color: Colors.white,
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Stack(children: [
-                  Container(
-                    margin: const EdgeInsets.symmetric(vertical: 20),
-                    width: 200,
-                    height: 200,
-                    child: ClipRRect(
-                        borderRadius: BorderRadius.circular(100),
-                        child: Image.network(
-                          MyUser.currentUser.profileImg,
-                        )),
-                  ),
-                  Positioned(
-                    bottom: 20,
-                    right: 20,
-                    child: IconButton(
-                      onPressed: () async {
-                        await profileDialog(context);
-                        setState(() {});
-                      },
-                      icon: const Icon(
-                        Icons.account_circle,
-                        size: 32,
+                Align(
+                  alignment: Alignment.center,
+                  child: Stack(children: [
+                    Container(
+                      margin: const EdgeInsets.symmetric(vertical: 20),
+                      width: 200,
+                      height: 200,
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(100),
+                          child: Image.network(
+                            MyUser.currentUser.profileImg,
+                          )),
+                    ),
+                    Positioned(
+                      bottom: 20,
+                      right: 20,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: CustomColors.disabledColor,
+                            borderRadius: BorderRadius.circular(40)),
+                        child: IconButton(
+                          onPressed: () async {
+                            await profileDialog(context);
+                            setState(() {});
+                          },
+                          icon: const Icon(
+                            FontAwesomeIcons.userEdit,
+                            size: 30,
+                            color: Colors.white,
+                          ),
+                        ),
                       ),
                     ),
+                  ]),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4),
+                  child: Text(
+                    "Name: ${MyUser.currentUser.name}",
+                    style: const TextStyle(fontSize: 20),
                   ),
-                ]),
-                Align(
-                    alignment: Alignment.bottomLeft,
-                    child: Text(
-                      "Name: ${MyUser.currentUser.name}",
-                      style: const TextStyle(fontSize: 20),
-                    )),
-                Align(
-                    alignment: Alignment.topLeft,
+                ),
+                Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 4),
                     child: Text(
                       "Email Id: ${MyUser.currentUser.email}",
                       style: const TextStyle(fontSize: 20),
                     )),
-                Align(
-                    alignment: Alignment.topLeft,
+                Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 4),
                     child: Text(
                       "Forms Created: ${MyUser.currentUser.forms.length}",
                       style: const TextStyle(fontSize: 20),
                     )),
-                Align(
+                Container(
                     alignment: Alignment.bottomRight,
+                    padding: const EdgeInsets.symmetric(vertical: 8),
                     child: TextButton(
                       child: const Text(
                         "report a problem?",
